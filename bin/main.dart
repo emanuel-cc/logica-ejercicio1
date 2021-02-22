5db825bd41b82411332d5070971e6c901453ccd7
Emanuel Antonio <Emanuel Antonio@DESKTOP-10ULSAH.(none)>
2021-02-21 21:08:17 -0600

    agregando lista dinámica

--- bin/main.dart
+++ bin/main.dart
@@ -6,21 +6,21 @@ main(List<String> arguments) {
   List<int> fila = [0,1,2,3];
   List<int> columna = [0,1,2,3];
 
-  // stdout.writeln('Ingresa el tamaño de la cuadrícula: ');
-  // int n = int.parse(stdin.readLineSync());
+  stdout.writeln('Ingresa el tamaño de la cuadrícula: ');
+  int n = int.parse(stdin.readLineSync());
 
-  for (var i = 0; i < fila.length; i++) {
-    for (var j = 0; j < columna.length; j++) {
+  for (var i = 0; i < n; i++) {
+    for (var j = 0; j < n; j++) {
       // print("(${i},${j})");
       //1 <= T <= 5000, 1 <= N,M <= 10^9
       // Se compara el primer valor de la fila cuando es igual a cero
-      if(columna[j] <= 0){
-        stdout.writeln("(${columna[j]},${fila[i]})");
+      if(j <= 0){
+        stdout.writeln("(${j},${i})");
         stdout.writeln("=>");
         // Se compara si ya se llegó al límite del tamaño de la fila para
         // para luego pasar a recorrer la columna
-      }else if(fila[i] <= fila.length){
-        stdout.writeln("(${fila[i]},${columna[j]})");
+      }else if(i <= n){
+        stdout.writeln("(${i},${j})");
       }
     }
   }
